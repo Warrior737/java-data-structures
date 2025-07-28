aee6e1f16d63c4e8427ae4708c9b693935dba39f
alonsoguerrero <alonsoguerrero710@gmail.com>
2025-07-28 16:51:38 -0600

    Binary Search | Traverse

--- src/data/structures/trees/BinarySearchTree.java
+++ src/data/structures/trees/BinarySearchTree.java
@@ -1,5 +1,9 @@
 package data.structures.trees;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinarySearchTree {
 
     Node root;
@@ -53,4 +57,152 @@ public class BinarySearchTree {
         }
         return false;
     }
+
+    // Recursive Methods
+
+    private boolean rContains(Node currentNode, int value) {
+        if(currentNode== null) return false;
+
+        if (currentNode.value == value) return true;
+
+        if(value < currentNode.value) {
+            return rContains(currentNode.left, value);
+        } else {
+            return rContains(currentNode.right, value);
+        }
+    }
+
+    public boolean rContains(int value) {
+        return rContains(root, value);
+    }
+
+
+    private Node rInsert(Node currentNode, int value) {
+        if(currentNode == null) return new Node(value);
+
+        if(value < currentNode.value) {
+            currentNode.left = rInsert(currentNode.left, value);
+        }else if (value > currentNode.value) {
+            currentNode.right = rInsert(currentNode.right, value);
+        }
+        return currentNode;
+    }
+
+    public void rInsert(int value) {
+        if (root == null) root = new Node(value);
+        rInsert(root,value);
+    }
+
+
+    public void deleteNode(int value) {
+        deleteNode(root, value);
+    }
+
+
+    private Node deleteNode(Node currentNode, int value) {
+        if (currentNode == null) return null;
+
+        if (value < currentNode.value) {
+            currentNode.left = deleteNode(currentNode.left, value);
+        } else if (value > currentNode.value) {
+            currentNode.right = deleteNode(currentNode.right, value);
+        } else {
+            if (currentNode.left == null && currentNode.right == null) {
+                currentNode = null;
+            } else if (currentNode.left == null) {
+                currentNode = currentNode.right;
+            } else if (currentNode.right == null) {
+                currentNode = currentNode.left;
+            } else {
+                int subTreeMin = minValue(currentNode.right);
+                currentNode.value = subTreeMin;
+                currentNode.right = deleteNode(currentNode.right, subTreeMin);
+            }
+        }
+        return currentNode;
+    }
+
+    public int minValue(Node currentNode) {
+        while (currentNode.left != null) {
+            currentNode = currentNode.left;
+        }
+        return currentNode.value;
+    }
+
+    public ArrayList<Integer> breadthFirstTraversal(){
+        Node currentNode = root;
+        Queue<Node> queue = new LinkedList<>();
+        ArrayList<Integer> results = new ArrayList<>();
+        queue.add(currentNode);
+
+        while(queue.size() > 0) {
+            currentNode = queue.remove();
+
+            results.add(currentNode.value);
+            if ( currentNode.left != null) {
+                queue.add(currentNode.left);
+            }
+            if(currentNode.right != null) {
+                queue.add(currentNode.right);
+            }
+        }
+    return results;
+    }
+
+    public ArrayList<Integer> DepthFirstSearchPreOrder() {
+        ArrayList<Integer> results = new ArrayList<>();
+
+        class Traverse {
+            Traverse(Node currentNode) {
+                results.add(currentNode.value);
+                if(currentNode.left != null) {
+                    new Traverse(currentNode.left);
+                }
+                if(currentNode.right != null) {
+                    new Traverse((currentNode.right));
+                }
+            }
+        }
+        new Traverse(root);
+        return results;
+    }
+
+    public ArrayList<Integer> depthFirstSearchPostOrder() {
+        ArrayList<Integer> results = new ArrayList<>();
+
+        class Traverse {
+            Traverse(Node currentNode) {
+                if(currentNode.left != null) {
+                    new Traverse(currentNode.left);
+                }
+                if(currentNode.right != null) {
+                    new Traverse(currentNode.right);
+                }
+                results.add(currentNode.value);
+            }
+        }
+        new Traverse(root);
+        return results;
+    }
+
+
+
+    public ArrayList<Integer> depthSearchInOrder() {
+        ArrayList<Integer> results = new ArrayList<>();
+        class Traverse {
+            Traverse(Node currentNode) {
+                if (currentNode.left != null) {
+                    new Traverse(currentNode.left);
+                }
+                results.add(currentNode.value);
+                if (currentNode.right != null) {
+                    new Traverse(currentNode.right);
+                }
+            }
+        }
+        new Traverse(root);
+        return results;
+    }
+
+
 }
--- src/data/structures/trees/Main.java
+++ src/data/structures/trees/Main.java
@@ -11,8 +11,6 @@ public class Main {
                   <-18->  <-27->  <-52->  <-82->
                            *new
          */
-
-
         BinarySearchTree myBST = new BinarySearchTree();
 
         myBST.insert(47);
@@ -24,14 +22,80 @@ public class Main {
 
         myBST.insert(27);
 
-
         System.out.println(myBST.root.left.right.value);
 
-        System.out.println(myBST.contains(27));
-        System.out.println(myBST.contains(17));
+        System.out.println("Non Recursive :" + myBST.contains(27));
+        System.out.println("Non Recursive :" + myBST.contains(17));
+
+        // Recursive Example
+
+        System.out.println("Recursive Contains 27: " + myBST.rContains(27));
+        System.out.println("Recursive Contains 17:" + myBST.rContains(17));
 
 
+        // Min Value
+        System.out.println(myBST.minValue(myBST.root));
+        System.out.println(myBST.minValue( myBST.root.right));
 
+
+        // Recursive Insert
+
+        BinarySearchTree myBSearchT = new BinarySearchTree();
+        myBSearchT.rInsert(2);
+        myBSearchT.rInsert(1);
+        myBSearchT.rInsert(3);
+
+        /*
+        Lines Above Create this Tree:
+                        2
+                       / \
+                      1   3
+         */
+
+        System.out.println("Root" + myBSearchT.root);
+        System.out.println("Root -> Left: " + myBSearchT.root.left.value);
+        System.out.println("Root -> Right: " + myBSearchT.root.right.value);
+
+        myBSearchT.deleteNode(2);
+
+                /*
+        Lines Above Create this Tree:
+                        2
+                       / \
+                      1   null
+         */
+
+        System.out.println("Delete Node ");
+        System.out.println("Root" + myBSearchT.root);
+        System.out.println("Root -> Left: " + myBSearchT.root.left.value);
+        System.out.println("Root -> Right: " + myBSearchT.root.right);
+
+
+        /*
+        Breadth First Search Method
+         */
+
+        System.out.println("Print Breadth First Search Traversal");
+        System.out.println(myBST.breadthFirstTraversal());
+
+        /*
+        Depth First Search Pre-Order
+         */
+        System.out.println(" Depth First Search Pre-Order ");
+        System.out.println( myBST.DepthFirstSearchPreOrder());
+
+
+        /*
+         Depth First Search Post Order
+         */
+        System.out.println("  Depth First Search Post Order ");
+        System.out.println(myBST.depthFirstSearchPostOrder());
+
+        /*
+         Depth First Search In Order
+         */
+        System.out.println("  Depth First Search In Order ");
+        System.out.println(myBST.depthSearchInOrder());
 
     }
 }
